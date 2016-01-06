287ae023139e426ff5bfef694ba9e8633ac71159
TheDeibo <deiboibbs@gmail.com>
2016-01-06 09:11:24 +0000

    The real v.2.0

--- src/ServerLoveMCPE/Main.php
+++ src/ServerLoveMCPE/Main.php
@@ -1,52 +1,42 @@
 <?php
 namespace ServerLoveMCPE;
-
-use pocketmine\plugin\PluginBase;
-
-use ServerLoveMCPE\handler\EventListener;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 
-use pocketmine\Player;
-use pocketmine\IPlayer;
-
-class Main extends PluginBase {
-    
-    public function onEnable()
-    {
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+class Main extends PluginBase implements Listener{
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder());
-        $this->nolove = new Config($this->getDataFolder()."nolove.txt", Config::ENUM);
+        $this->nolove = new Config($this->getDataFolder()."nolove.yml", Config::YAML);
         $this->saveDefaultConfig();
         $this->getLogger()->info(TextFormat::RED . " Yayyy, ServerLoveMCPE is ready for love on Version ".$this->getDescription()->getVersion());
     }
     
-    public function onDisable()
-    {
+    public function onDisable(){
         $this->nolove->save();
     }
     
-    /**
-     * @param CommandSender $sender
-     * @param Command $command
-     * @param string $label
-     * @param array $args
-     * 
-     * @return bool
-     */
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-        switch(strtolower($command->getName()){
+        switch($command->getName()){
+/**
+*
+*                                         LOVE
+*
+**/
             case "love":
                 if(!(isset($args[0]))){
                     return false;
                 }
                 $loved = array_shift($args);
                 if($this->nolove->exists(strtolower($loved))){
-                    $sender->sendMessage("§5 Sorry," . $loved . " §5  is not interested in you.");
+                    $sender->sendMessage("§5 Sorry, " . $loved . "§5 is not looking to love anyone right now.");
                     return true;
                 }else{
                     $lovedPlayer = $this->getServer()->getPlayer($loved);
@@ -55,14 +45,43 @@ class Main extends PluginBase {
                         if(isset($args[0])){
                             $lovedPlayer->sendMessage("Reason: " . implode(" ", $args));
                         }
-                        $sender->sendMessage("§5So you love " . $loved . "?§5 Awww, thats nice.");
-                        $this->getServer()->broadcastMessage("§a" . $sender->getName() . " §5is in love with §a" . $loved . "§5.");
+                        $sender->sendMessage("§5So you love §a" . $loved . "?§5 Awww, thats nice");
+                        $this->getServer()->broadcastMessage("§a" . $sender->getName() . " §dis in love with §a" . $loved . "§d.");
                         return true;
                     }else{
-                        $sender->sendMessage($loved . "§5 is not avalible for love. #shameful. §5 Basically, " . $loved . " §5is not interested in you.");
+                        $sender->sendMessage($loved . "§5 is not avalible for love. #shameful. §5 Basically, §a" . $loved . "§5 does not exist, or is not online.");
                         return true;
                     }
                 }
+/**
+*
+*                                 BREAKUP
+*
+**/
+                break;
+            case "breakup":
+                if(!(isset($args[0]))){
+                    return false;
+                }
+                $loved = array_shift($args);
+                    $lovedPlayer = $this->getServer()->getPlayer($loved);
+                    if($lovedPlayer !== null and $lovedPlayer->isOnline()){
+                        $lovedPlayer->sendMessage($sender->getName()."§5 has broken up with you!");
+                        if(isset($args[0])){
+                            $lovedPlayer->sendMessage("Reason: " . implode(" ", $args));
+                        }
+                        $sender->sendMessage("§5You have broken up with §a" . $loved . "§5.");
+                        $this->getServer()->broadcastMessage("§a" . $sender->getName() . " §dhas broken up with §a" . $loved . "§d.");
+                        return true;
+                    }else{
+                        $sender->sendMessage($loved . "§5 is not avalible for a breakup. Basically, §a" . $loved . "§5 does not exist, or is not online.");
+                        return true;
+                    }
+/**
+*
+*                                      NOLOVE
+*
+**/
             break;
             case "nolove":
                 if(!(isset($args[0]))){
@@ -79,13 +98,17 @@ class Main extends PluginBase {
                 }else{
                     return false;
                 }
+/**
+*
+*                                   SERVERLOVE
+*
+**/
             break;
             case "serverlove":
-                $sender->sendMessage("§5[ServerLoveMCPE] ServerLoveMCPE is a plugin that brings a little love to a server. ");
                 $sender->sendMessage("§5[ServerLoveMCPE] Original ServerLove (For MCPC )  Made By ratchetgame98 ");
-                $sender->sendMessage(" ");
                 $sender->sendMessage("§d[ServerLoveMCPE] Usage: /love <playerName>");
-                $sender->sendMessage("§5[ServerLoveMCPE] Usage: /nolove <nolove|love> ");
+                $sender->sendMessage("§d[ServerLoveMCPE] Usage: /breakup <playerName>");
+                $sender->sendMessage("§5[ServerLoveMCPE] Usage: /nolove <nolove / love> ");
                 $sender->sendMessage("§d[ServerLoveMCPE] Happy Loving!");
                 return true;
             break;
@@ -94,67 +117,4 @@ class Main extends PluginBase {
         }
     return false;
     }
-    // TODO Remove onCommand() and use $commandMap->registerCommand()
-    
-    
-    /**      ___       ______    __  
-    *       /   \     |   _  \  |  | 
-    *      /  ^  \    |  |_)  | |  | 
-    *     /  /_\  \   |   ___/  |  | 
-    *    /  _____  \  |  |      |  | 
-    *   /__/     \__\ | _|      |__| 
-    */
-    
-    /**
-     * @param IPlayer $player
-     * 
-     * @return bool
-     */
-    public function isMarried(IPlayer $player)
-    {
-     // TODO    
-    }
-    
-    /**
-     * @param IPlayer $player
-     * 
-     * @return IPlayer|null
-     */
-    public function getMarriagePartner(IPlayer $player){
-        // TODO
-    }
-    /**
-     * @param IPlayer $wife
-     * @param IPlayer $husband
-     * @description Returns true on success, false on fail
-     * 
-     * @return bool
-     */
-    public function merry(IPlayer $wife, IPlayer $husband)
-    {
-     // TODO
-    }
-    
-    /**
-     * @param IPlayer $player
-     * @description Returns true if $player is dating someone or false if not are is married
-     * 
-     * @return bool
-     */
-    public function isDating(IPlayer $player)
-    {
-     // TODO
-    }
-    
-    /**
-     * @param IPlayer $player
-     * 
-     * @return IPlayer|null
-     */
-    public function getDatePartner(IPlayer $player)
-    {
-     // TODO
-     // Better name
-    }
-    
 }
