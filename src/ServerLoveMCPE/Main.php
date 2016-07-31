f79b16ec5e03b89c8e28146e4de3eec8fc62927c
Driesboy <coenendries3@gmail.com>
2016-07-31 11:10:20 +0200

    Update Main.php

--- src/ServerLoveMCPE/Main.php
+++ src/ServerLoveMCPE/Main.php
@@ -263,7 +263,6 @@ class Main extends PluginBase implements Listener{
 		$data->set("type", $type); 
 		$data->save();
 		$pet->setOwner($player);
-		$pet->setDataFlag(self::DATA_FLAG_BABY);
 		$pet->spawnToAll();
 		return $pet; 
 	}
